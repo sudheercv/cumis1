87f9b8e35cf4afec2f1d571506d232b02f15b71c
sudheercv <sudheer.chinthu@gmail.com>
2023-05-25 01:53:10 +0530

    adding maven project

---
+++ cumis/src/main/java/cumis/CumisApplication.java
@@ -0,0 +1,13 @@
+package cumis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CumisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CumisApplication.class, args);
+	}
+
+}
---
+++ cumis/src/test/java/cumis/CumisApplicationTests.java
@@ -0,0 +1,13 @@
+package cumis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CumisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
